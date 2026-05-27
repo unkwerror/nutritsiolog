b6a11a268e36f262637fed41831fbfc6ab6b8a5d
taykee <i.grishanow@gmail.com>
2026-05-28 00:05:37 +0700

    fix socks

--- api/src/core/proxy.ts
+++ api/src/core/proxy.ts
@@ -2,7 +2,8 @@ import { setGlobalDispatcher } from 'undici'
 import { socksDispatcher } from 'fetch-socks'
 import { config } from '../core/config.js'
 
-if (config.SOCKS_PROXY) {
+// Proxy нужен только для Gemini (заблокирован в РФ). Yandex работает напрямую.
+if (config.SOCKS_PROXY && config.OCR_PROVIDER === 'gemini') {
     const proxyUrl = new URL(config.SOCKS_PROXY)
     setGlobalDispatcher(
         socksDispatcher({
